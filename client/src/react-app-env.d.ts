d08fe1dd25cc6905f6b4a2c95c030c252859966d
christianlinzatti <44360089+christianlinzatti@users.noreply.github.com>
2026-08-07 21:15:46 +0200

    Update react-app-env.d.ts

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,10 @@
-declare module 'lrc-kit';
+declare module 'lrc-kit' {
+  export interface Lyric {
+    timestamp: number;
+    content: string;
+    [key: string]: any;
+  }
+
+  export function parse(text: string): Lyric[];
+  export function stringify(lyrics: Lyric[]): string;
+}
