b025e683fdad8efd219deacc9d2c6323cd622b78
christianlinzatti <christian.linzatti@gmail.com>
2026-08-18 22:09:28 +0200

    lyric

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,54 @@
-// src/api/index.ts
+// C:\Users\Christian\spotify-lyics-viewer\spotify-lyrics-viewer\src\api\index.ts
 
-// Wenn getLyrics in src/api/lyrics.ts (oder client.ts) liegt:
-export * from "./lyrics";
+import { IFoundLyrics, ITokenExpiryPair } from "../../../src/dto";
+import Config from "../config";
 
-// Falls du weitere API-Module hast (z. B. spotify.ts, genius.ts, lrclib.ts):
-export * from "./genius";
-export * from "./lrclib";
-export * from "./spotify";
+export function getLyrics(
+  artists: string[],
+  title: string,
+  albumName: string,
+  duration: number
+): Promise<IFoundLyrics | null> {
+  const parameters = new URLSearchParams();
+  parameters.append("title", title);
+  for (const artist of artists) {
+    parameters.append("artists", artist);
+  }
+  parameters.append("albumName", albumName);
+  parameters.append("duration", duration.toString());
+
+  return fetch(`${Config.api.root}/api/lyrics?${parameters}`).then(r =>
+    r.status === 200 ? r.json() : null
+  );
+}
+
+export function spotifyGetCurrentToken(): Promise<ITokenExpiryPair | null> {
+  return fetch(`${Config.api.root}/api/spotify/token`, {
+    credentials: "include"
+  }).then(r => {
+    if (r.status === 200) {
+      return r.json();
+    } else {
+      return null;
+    }
+  });
+}
+
+export function spotifyRefreshToken(): Promise<ITokenExpiryPair | null> {
+  return fetch(`${Config.api.root}/api/spotify/refresh-token`, {
+    credentials: "include"
+  }).then(r => {
+    if (r.status === 200) {
+      return r.json();
+    } else {
+      return null;
+    }
+  });
+}
+
+export function deleteSession(): Promise<Response> {
+  return fetch(`${Config.api.root}/api/session`, {
+    credentials: "include",
+    method: "DELETE"
+  });
+}
